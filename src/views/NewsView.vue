28e7a37ffced0d64ebe771107278a19ef5831c07
dae-eun <dannynam@iocentre.biz>
2022-09-28 10:31:57 +0900

    test

---
+++ src/views/NewsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="user in users" :key="user">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchNewsList } from '../api/index.js';
+export default {
+  data(){
+    return{
+      users:[]
+    }
+  },
+  created(){
+    var vm = this;
+    fetchNewsList()
+      .then(function(response){
+        vm.users = response.data;
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+  }
+}
+</script>
+
+<style>
+
+</style>
